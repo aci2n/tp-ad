99385400117a29d964ed39ae864e96299c3c8fa1
Gabriel <GABROL@GABROL.GAB>
2015-06-21 23:42:31 -0300

    DO IT.

---
+++ TP_AD_Web/src/model/views/misc/TamanoView.java
@@ -0,0 +1,28 @@
+package model.views.misc;
+
+
+public class TamanoView {
+	private Float profundidad;
+	private Float alto;
+	private Float ancho;
+	
+	public Float getProfundidad() {
+		return profundidad;
+	}
+	public void setProfundidad(Float profundidad) {
+		this.profundidad = profundidad;
+	}
+	public Float getAlto() {
+		return alto;
+	}
+	public void setAlto(Float alto) {
+		this.alto = alto;
+	}
+	public Float getAncho() {
+		return ancho;
+	}
+	public void setAncho(Float ancho) {
+		this.ancho = ancho;
+	}
+	
+}
---
+++ TP_AD_Web/src/model/views/productos/ItemProductoView.java
@@ -0,0 +1,21 @@
+package model.views.productos;
+
+
+public class ItemProductoView {
+	private ProductoView producto;
+	private float cantidad;
+	
+	public ProductoView getProducto() {
+		return producto;
+	}
+	public void setProducto(ProductoView producto) {
+		this.producto = producto;
+	}
+	public float getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(float cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
