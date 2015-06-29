710f3f82f7d91f39f2986bdbe79f185914feb673
alvarocalace <alvicala_10@hotmail.com>
2015-06-29 17:12:20 -0300

    q

--- TP_AD_Web/src/swing/AltaProveedor.java
+++ TP_AD_Web/src/swing/AltaProveedor.java
@@ -25,7 +25,7 @@ public class AltaProveedor extends GenericJFrame {
 	}
 
 	protected void inicializar() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 175);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
